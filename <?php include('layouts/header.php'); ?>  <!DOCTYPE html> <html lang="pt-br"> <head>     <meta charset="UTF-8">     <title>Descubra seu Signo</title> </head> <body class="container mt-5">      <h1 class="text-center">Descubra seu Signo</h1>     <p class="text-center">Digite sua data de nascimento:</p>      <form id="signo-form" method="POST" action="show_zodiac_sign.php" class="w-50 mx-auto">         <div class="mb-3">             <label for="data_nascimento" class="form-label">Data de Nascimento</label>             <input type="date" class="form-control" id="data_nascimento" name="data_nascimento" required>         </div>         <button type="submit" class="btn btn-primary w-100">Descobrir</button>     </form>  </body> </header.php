5b12bbaaeeb1a04ca2d634cb1712b56f2bddc1dd
bsbwanda <160541364+bsbwanda@users.noreply.github.com>
2026-04-21 06:00:40 -0300

    Create header.php
    o arquivo dentro da pasta layouts

---
+++ <?php include('layouts/header.php'); ?>  <!DOCTYPE html> <html lang="pt-br"> <head>     <meta charset="UTF-8">     <title>Descubra seu Signo</title> </head> <body class="container mt-5">      <h1 class="text-center">Descubra seu Signo</h1>     <p class="text-center">Digite sua data de nascimento:</p>      <form id="signo-form" method="POST" action="show_zodiac_sign.php" class="w-50 mx-auto">         <div class="mb-3">             <label for="data_nascimento" class="form-label">Data de Nascimento</label>             <input type="date" class="form-control" id="data_nascimento" name="data_nascimento" required>         </div>         <button type="submit" class="btn btn-primary w-100">Descobrir</button>     </form>  </body> </header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Aplicação Signos</title>
+    <!-- Link do Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" 
+          rel="stylesheet" 
+          integrity="sha384-gH2yIJqKdNHPEqOn4Mqa/HGKIhSKIHeL5AyhkYV8159USAR6csBvApHNI/VI1Bx" 
+          crossorigin="anonymous">
+    <!-- Seu CSS personalizado -->
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
